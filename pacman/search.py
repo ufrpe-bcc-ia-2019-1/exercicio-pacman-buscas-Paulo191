7781cbbf89d5f935f8c1d65f2d4eb861a7347218
Paulo <paullo191sergio@gmail.com>
2019-05-09 21:22:45 -0300

    entrega final

--- pacman/search.py
+++ pacman/search.py
@@ -86,56 +86,52 @@ def depthFirstSearch(problem):                                                  
     '''Tem que ser usado um tipo abstrato de dados do tipo PILHA,
     porque na busca em profundidade, os ultimos nohs que entram
     sao os primeiros a serem visitados e sairem da fronteira.'''
-
     
-    visitados = {} "Eh um dicionario pois contem os nohs que foram retirados da PILHA e o caminho de como foram obtidos"
+    visitados = {} 
 
-    solucao = [] "Uma lista de nohs representando o caminho que o agente ira seguir"
+    solucao = [] 
 
-    pais = {} "Dicionario do qual 'solucao' vai ser preenchida com as direcoes"
+    pais = {} 
 
     "Criamos o objeto 'fronteira' da classe Stack()"
-    fronteira = util.Stack() "'fronteira' recebe a devolucao do método Stack() que cria um tipo abstrato de dados do tipo PILHA conforme implementada na classe util"
-    "a fronteira sera uma tripla (noh na fronteira, direcao, custo)
+    fronteira = util.Stack() 
 
     "A funcao getStartState() devolve uma dupla (x, y)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    nohInicial = problem.getStartState() "Inicializa o problema com seu ESTADO INICIAL, que sera uma dupla (x, y)"
+    nohInicial = problem.getStartState()
+
+    nohSolucao = 0
+
+    visitados[nohInicial] = 'Indefinida'
     
-    fronteira.push((nohInicial, 'Indefinida', 0)) "Coloca o estado inicial na pilha, ficando algo como [(x, y), 'Indefinida', 0]"
+    fronteira.push((nohInicial, 'Indefinida', 0)) 
     
 
     if (problem.isGoalState(nohInicial) == True):
-        return solucao "A lista 'solucao' esta vazia. Devolvema-la aqui porque se o noh inicial for a solucao,"
-                        "isso significa que o agente simplesmente nao vai andar, isto e, seguira um conjunto de instrucoes vazio"
+        return solucao 
 
     objetivo = False
         
-    while (fronteira.isEmpty == False and objetivo == False):
-        noh = fronteira.pop() "Criamos 'noh', que é uma tripla (noh na fronteira, direcao, custo)"
-        visitados[noh[0]] = noh[1] "'noh[0]' == 'noh na fronteira' (que é uma dupla (x, y)) e 'noh[1]' == 'direcao' (que é um nome da direção, ex.: 'South'), ou seja,"
-                                    "o dicionario 'visitados' tem como chave as coordenadas do noh e como valores a direcao"
+    while (fronteira.isEmpty() == False and objetivo == False):
+        noh = fronteira.pop() 
+        visitados[noh[0]] = noh[1] 
         
         "Verifica se o noh tirado da fronteira(visitado) eh o objetivo"
         if(problem.isGoalState(noh[0])):
-            nohSolucao = noh[0] "'nohSolucao' é uma dupla (x, y) que é o objetivo do problema"
-            objetivo = True "Para esse 'while' não ser mais executado"
-            break "Para o laco 'for' abaixo não ser executado, pois ja chegamos no objetivo"
+            nohSolucao = noh[0] 
+            objetivo = True 
+            break 
 
-        for sucessor in problem.getSuccessors(noh[0]): "'sucessor' eh uma TRIPLA!!!"
-           if(sucessor[0] not in visitados.keys()): "por isso pegamos 'sucessor[0]', pois estamos interessados apenas em suas coordenadas (x, y)"
-              fronteira.push(sucessor) "bota cada 'sucessor' na pilha da 'fronteira'"
-              pais[sucessor[0]] = noh[0]
+        for sucessor in problem.getSuccessors(noh[0]): 
+           if(sucessor[0] not in visitados.keys()):
+               fronteira.push(sucessor)
+               pais[sucessor[0]] = noh[0]
 
     while (nohSolucao in pais.keys()):
-        paiDoNohSolucao = pai[nohSolucao]
-        solucao.insert(0, paiDoNohSolucao[1])
+        paiDoNohSolucao = pais[nohSolucao]
+        solucao.insert(0, visitados[nohSolucao])
         nohSolucao = paiDoNohSolucao
 
     return solucao
-              
-    '''print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())'''
     
     util.raiseNotDefined()
 
@@ -147,47 +143,48 @@ def breadthFirstSearch(problem):                                                
     *** YOUR CODE HERE ***
     """
 
-    visitados = {} "Eh um dicionario pois contem os nohs que foram retirados da PILHA e o caminho de como foram obtidos"
+    visitados = {}
 
-    solucao = [] "Uma lista de nohs representando o caminho que o agente ira seguir"
+    solucao = [] 
 
-    pais = {} "Dicionario do qual 'solucao' vai ser preenchida com as direcoes"
+    pais = {} 
 
     fronteira = util.Queue()
 
     "A funcao getStartState() devolve uma dupla (x, y)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    nohInicial = problem.getStartState() "Inicializa o problema com seu ESTADO INICIAL, que sera uma dupla (x, y)"
+    nohInicial = problem.getStartState()
+
+    nohSolucao = 0
+
+    visitados[nohInicial] = 'Indefinida'
     
-    fronteira.push((nohInicial, 'Indefinida', 0)) "Coloca o estado inicial na pilha, ficando algo como [(x, y), 'Indefinida', 0]"
+    fronteira.push((nohInicial, 'Indefinida', 0)) 
     
 
     if (problem.isGoalState(nohInicial) == True):
-        return solucao "A lista 'solucao' esta vazia. Devolvema-la aqui porque se o noh inicial for a solucao,"
-                        "isso significa que o agente simplesmente nao vai andar, isto e, seguira um conjunto de instrucoes vazio"
+        return solucao 
 
     objetivo = False
         
-    while (fronteira.isEmpty == False and objetivo == False):
-        noh = fronteira.pop() "Criamos 'noh', que é uma tripla (noh na fronteira, direcao, custo)"
-        visitados[noh[0]] = noh[1] "'noh[0]' == 'noh na fronteira' (que é uma dupla (x, y)) e 'noh[1]' == 'direcao' (que é um nome da direção, ex.: 'South'), ou seja,"
-                                    "o dicionario 'visitados' tem como chave as coordenadas do noh e como valores a direcao"
+    while (fronteira.isEmpty() == False and objetivo == False):
+        noh = fronteira.pop() 
+        visitados[noh[0]] = noh[1] 
         
         "Verifica se o noh tirado da fronteira(visitado) eh o objetivo"
         if(problem.isGoalState(noh[0])):
-            nohSolucao = noh[0] "'nohSolucao' é uma dupla (x, y) que é o objetivo do problema"
-            objetivo = True "Para esse 'while' não ser mais executado"
-            break "Para o laco 'for' abaixo não ser executado, pois ja chegamos no objetivo"
+            nohSolucao = noh[0] 
+            objetivo = True 
+            break 
 
         #############POR QUE VERIFICA SE NAO ESTA EM 'PAIS' TAMBEM????????????????????????????????????????????????????????
-        for sucessor in problem.getSuccessors(noh[0]): "'sucessor' eh uma TRIPLA!!!"
-           if(sucessor[0] not in visitados.keys() and sucessor[0] not in pais.keys()): "por isso pegamos 'sucessor[0]',"
-                                                                                        "pois estamos interessados apenas em suas coordenadas (x, y)"
-              fronteira.push(sucessor) "bota cada 'sucessor' na pilha da 'fronteira'"
-              pais[sucessor[0]] = noh[0]
+        for sucessor in problem.getSuccessors(noh[0]): 
+           if(sucessor[0] not in visitados.keys() and sucessor[0] not in pais.keys()):
+               fronteira.push(sucessor)
+               pais[sucessor[0]] = noh[0]
 
     while (nohSolucao in pais.keys()):
-        paiDoNohSolucao = pai[nohSolucao]
-        solucao.insert(0, paiDoNohSolucao[1])
+        paiDoNohSolucao = pais[nohSolucao]
+        solucao.insert(0, visitados[nohSolucao])
         nohSolucao = paiDoNohSolucao
 
     return solucao
@@ -200,47 +197,55 @@ def uniformCostSearch(problem):                                                 
     *** YOUR CODE HERE ***
     """
 
-    visitados = {} "Eh um dicionario pois contem os nohs que foram retirados da PILHA e o caminho de como foram obtidos"
+    visitados = {} 
 
-    solucao = [] "Uma lista de nohs representando o caminho que o agente ira seguir"
+    solucao = [] 
 
-    pais = {} "Dicionario do qual 'solucao' vai ser preenchida com as direcoes"
+    pais = {} 
 
     fronteira = util.PriorityQueue()
 
-    custo = {} "Dicionario que guarda os custos de um caminho. Vai servir para comparar custos de um mesmo caminho, para que o de custo menor seja escolhido"
+    custo = {}
 
     "A funcao getStartState() devolve uma dupla (x, y)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    nohInicial = problem.getStartState() "Inicializa o problema com seu ESTADO INICIAL, que sera uma dupla (x, y)"
-    
-    fronteira.push((nohInicial, 'Indefinida', 0), 0) "Coloca o estado inicial na pilha, ficando algo como [(x, y), 'Indefinida', 0]"
-    
+    nohInicial = problem.getStartState()
 
+    nohSolucao = 0
+    
+    fronteira.push((nohInicial, 'Indefinida', 0), 0)
+
+    custo[nohInicial] = 0
+
+    visitados[nohInicial] = 'Indefinida'
+    
     if (problem.isGoalState(nohInicial) == True):
-        return solucao "A lista 'solucao' esta vazia. Devolvema-la aqui porque se o noh inicial for a solucao,"
-                        "isso significa que o agente simplesmente nao vai andar, isto e, seguira um conjunto de instrucoes vazio"
+        return solucao 
 
     objetivo = False
         
-    while (fronteira.isEmpty == False and objetivo == False):
-        noh = fronteira.pop() "Criamos 'noh', que é uma tripla (noh na fronteira, direcao, custo)"
-        visitados[noh[0]] = noh[1] "'noh[0]' == 'noh na fronteira' (que é uma dupla (x, y)) e 'noh[1]' == 'direcao' (que é um nome da direção, ex.: 'South'), ou seja,"
-                                    "o dicionario 'visitados' tem como chave as coordenadas do noh e como valores a direcao"
+    while (fronteira.isEmpty() == False and objetivo == False):
+        noh = fronteira.pop() 
+        visitados[noh[0]] = noh[1] 
         
         "Verifica se o noh tirado da fronteira(visitado) eh o objetivo"
         if(problem.isGoalState(noh[0])):
-            nohSolucao = noh[0] "'nohSolucao' é uma dupla (x, y) que é o objetivo do problema"
-            objetivo = True "Para esse 'while' não ser mais executado"
-            break "Para o laco 'for' abaixo não ser executado, pois ja chegamos no objetivo"
+            nohSolucao = noh[0] 
+            objetivo = True 
+            break 
 
-        for sucessor in problem.getSuccessors(noh[0]): "'sucessor' eh uma TRIPLA!!!"
-           if(sucessor[0] not in visitados.keys()): "por isso pegamos 'sucessor[0]', pois estamos interessados apenas em suas coordenadas (x, y)"
-              fronteira.push(sucessor) "bota cada 'sucessor' na pilha da 'fronteira'"
-              pais[sucessor[0]] = noh[0]
+        for sucessor in problem.getSuccessors(noh[0]): 
+           if(sucessor[0] not in visitados.keys()): 
+               prioridade = noh[2] + sucessor[2]
+
+               if (sucessor[0] in custo.keys()):
+                  if custo[sucessor[0]] <= prioridade:
+                           fronteira.push((sucessor[0], sucessor[1], prioridade), prioridade)
+                           custo[sucessor[0]] = prioridade
+                           pais[sucessor[0]] = noh[0]
 
     while (nohSolucao in pais.keys()):
-        paiDoNohSolucao = pai[nohSolucao]
-        solucao.insert(0, paiDoNohSolucao[1])
+        paiDoNohSolucao = pais[nohSolucao]
+        solucao.insert(0, visitados[nohSolucao])
         nohSolucao = paiDoNohSolucao
 
     return solucao
@@ -257,51 +262,6 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):                                  ############################## IMPLEMENTAR ESSA!!! ##################################
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-
-    visitados = {} "Eh um dicionario pois contem os nohs que foram retirados da PILHA e o caminho de como foram obtidos"
-
-    solucao = [] "Uma lista de nohs representando o caminho que o agente ira seguir"
-
-    pais = {} "Dicionario do qual 'solucao' vai ser preenchida com as direcoes"
-
-    #Criamos o objeto 'fronteira' da classe Stack()
-    fronteira = util.Stack() "'fronteira' recebe a devolucao do método Stack() que cria um tipo abstrato de dados do tipo PILHA conforme implementada na classe util"
-    "a fronteira sera uma tripla (noh na fronteira, direcao, custo)
-
-    #A funcao getStartState() devolve uma dupla (x, y)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    nohInicial = problem.getStartState() "Inicializa o problema com seu ESTADO INICIAL, que sera uma dupla (x, y)"
-    
-    fronteira.push((nohInicial, 'Indefinida', 0)) "Coloca o estado inicial na pilha, ficando algo como [(x, y), 'Indefinida', 0]"
-    
-
-    if (problem.isGoalState(nohInicial) == True):
-        return solucao "A lista 'solucao' esta vazia. Devolvema-la aqui porque se o noh inicial for a solucao,"
-                        "isso significa que o agente simplesmente nao vai andar, isto e, seguira um conjunto de instrucoes vazio"
-
-    objetivo = False
-        
-    while (fronteira.isEmpty == False and objetivo == False):
-        noh = fronteira.pop() "Criamos 'noh', que é uma tripla (noh na fronteira, direcao, custo)"
-        visitados[noh[0]] = noh[1] "'noh[0]' == 'noh na fronteira' (que é uma dupla (x, y)) e 'noh[1]' == 'direcao' (que é um nome da direção, ex.: 'South'), ou seja,"
-                                    "o dicionario 'visitados' tem como chave as coordenadas do noh e como valores a direcao"
-        
-        #Verifica se o noh tirado da fronteira(visitado) eh o objetivo
-        if(problem.isGoalState(noh[0])):
-            nohSolucao = noh[0] "'nohSolucao' é uma dupla (x, y) que é o objetivo do problema"
-            objetivo = True "Para esse 'while' não ser mais executado"
-            break "Para o laco 'for' abaixo não ser executado, pois ja chegamos no objetivo"
-
-        for sucessor in problem.getSuccessors(noh[0]): "'sucessor' eh uma TRIPLA!!!"
-           if(sucessor[0] not in visitados.keys(): "por isso pegamos 'sucessor[0]', pois estamos interessados apenas em suas coordenadas (x, y)"
-              fronteira.push(sucessor) "bota cada 'sucessor' na pilha da 'fronteira'"
-              pais[sucessor[0]] = noh[0]
-
-    while (nohSolucao in pais.keys()):
-        paiDoNohSolucao = pai[nohSolucao]
-        solucao.insert(0, paiDoNohSolucao[1])
-        nohSolucao = paiDoNohSolucao
-
-    return solucao
               
     util.raiseNotDefined()
 
